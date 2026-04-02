51239993553de969fdf7dbda18a96d8ffdb6da9e
maaquize5 <maaquize5@umsa.bo>
2026-04-02 16:14:50 -0400

    Eliminar archivo temporal script-temp.js

--- script-temp.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const html = fs.readFileSync('index.html', 'utf8');
-const p1 = html.indexOf('<section id="hero"');
-const p2 = html.indexOf('</section>', p1);
-console.log(html.substring(p1, p2 + 10));
